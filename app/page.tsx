05601662033f3cdb9fcc99fbccd10f7d07f9bb22
dionyichia <dionyi2002@gmail.com>
2025-10-07 20:32:34 -0700

    added user-groups dir, added page.tsx to render user groups pull from supabase, prettier and lint changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
 import { CSSProperties } from "react";
 import Image from "next/image";
+import UserGroupsPage from "@/app/user_groups/page";
 import BPLogo from "@/assets/images/bp-logo.png";
-import UserGroupsPage from "@/app/user_groups/page"
 
 export default function Home() {
   return (
     <main style={mainStyles}>
       <Image style={imageStyles} src={BPLogo} alt="Blueprint Logo" />
       <p>Open up app/page.tsx to get started!</p>
-      <UserGroupsPage/>
+      <UserGroupsPage />
     </main>
   );
 }
--- app/user_groups/page.tsx
+++ app/user_groups/page.tsx
@@ -1,53 +1,45 @@
-"use client"
+"use client";
 
-import supabase from "@/actions/supabase/client"
+import { useEffect, useState } from "react";
+import supabase from "@/actions/supabase/client";
 import { UserGroup } from "@/types/schema";
-import { useState, useEffect } from "react";
 
 const UserGroupsPage = () => {
+  const [userGroups, setUserGroups] = useState<UserGroup[]>([]);
 
-  const [userGroups, setUserGroups] = useState<UserGroup[]>([])
-  
   useEffect(() => {
     async function fetchUserGroups() {
       try {
         // Pull data
-        const { data, error } = await supabase
-        .from('user_group')
-        .select('*')
+        const { data, error } = await supabase.from("user_group").select("*");
 
-        if (error) throw error
+        if (error) throw error;
 
         // Process data
         if (data) {
-          const mapped = data.map((usergrp) => ({
+          const mapped = data.map(usergrp => ({
             id: usergrp.user_group_id,
-            user_group_name: usergrp.user_group_name
-          }))
+            user_group_name: usergrp.user_group_name,
+          }));
 
-          setUserGroups(mapped)
-        }      
+          setUserGroups(mapped);
+        }
       } catch (error) {
-        console.log("Error fetching orgs data from supabase API: ", error)
+        console.log("Error fetching orgs data from supabase API: ", error);
       }
     }
-    
-    fetchUserGroups()
-  }, [])
+
+    fetchUserGroups();
+  }, []);
 
   return (
     <div>
-      <h1>User Groups</h1>      
-      {
-        userGroups.map((usergrp, index) => (
-          <div key={index}>
-            {usergrp.user_group_name}
-          </div>
-        ))
-      }
+      <h1>User Groups</h1>
+      {userGroups.map((usergrp, index) => (
+        <div key={index}>{usergrp.user_group_name}</div>
+      ))}
     </div>
   );
 };
 
-
-export default UserGroupsPage
+export default UserGroupsPage;
