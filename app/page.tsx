e8c98fc64992df7efe7f2be7825b144142b1a4f0
Cody Garcia <czgarcia@berkeley.edu>
2025-10-08 22:58:51 -0700

    some good functionality, basic adding new roles and phases, nothing saving yet tho

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { CSSProperties } from "react";
 import Image from "next/image";
 import BPLogo from "@/assets/images/bp-logo.png";
+import NewTemplatePage from "./templates/page";
 
 export default function Home() {
   return (
--- app/templates/page.tsx
+++ app/templates/page.tsx
@@ -1,18 +1,11 @@
-import { NewRole } from "./components/components";
+"use client";
+
+import { NewRole, NewPhase } from "./components/components";
 import { useState } from "react";
 
-export default function Page() {
-  const submitScenario = () => {
-    // go to add roles
-    return
-  }
-
-  return(
-    <>
-      <h1>New Template Page</h1>
-      {/* scenario title */}
-      {/* scenario description */}
-      <form onSubmit={submitScenario}>
+function ScenarioStep() {
+  return (
+    <form>
         <label>
           Scenario Title:
           <input type="text" name="scenario_title" placeholder="" />
@@ -23,13 +16,73 @@ export default function Page() {
         </label>
         <input type="submit" value="Add Roles" />
       </form>
+  )
+}
 
+function RolesStep() {
+  const [roles, SetRoles] = useState([<NewRole/>]);
+
+  const newRole = () => {
+    SetRoles([...roles, <NewRole/>])
+  }
+  return (
+    <>
+      {roles.map((role) => (
+        role
+      ))}
+      <button onClick={newRole}>New Role</button>
+      <button>Add Phases</button>
+    </>
+  )
+}
+
+function PhasesStep() {
+  let roles = ['role1', 'role2', 'role3']
+
+  const [phases, SetPhases] = useState([<NewPhase roles={roles}/>]);
+
+  const newPhase = () => {
+    SetPhases([...phases, <NewPhase roles={roles}/>])
+  }
+  return (
+    <>
+      {phases.map((phase) => (
+        phase
+      ))}
+      <button onClick={newPhase}>New Phase</button>
+      <button>Finish Template</button>
+    </>
+  )
+}
+
+
+
+export default function NewTemplatePage() {
+  const submitScenario = () => {
+    // go to add roles
+    return
+  }
+
+//   const TABS = [
+//   { id: "intro",       label: "Introduction",  component: IntroTab },
+//   { id: "experience",  label: "Work Experience", component: ExperienceTab },
+//   { id: "education",   label: "Education",     component: EducationTab },
+//   { id: "review",      label: "Review & Submit", component: ReviewTab },
+// ];
+  return(
+    <>
+      <h1>New Template Page</h1>
+      {/* scenario title */}
+      {/* scenario description */}
+      <ScenarioStep/>
       {/* next --------> */}
+      <RolesStep/>
       {/* add roles */}
         {/* role name */}
         {/* role description */}
         {/* OPTIONAL: role requirements? */}
       {/* next --------> */}
+      <PhasesStep/>
       {/* add phases */}
         {/* phase title */}
         {/* phase description */}
@@ -38,3 +91,4 @@ export default function Page() {
     </>
   )
 }
+
