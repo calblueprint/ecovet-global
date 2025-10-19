dde0bbb2a8feb79306475f2e49f79968d4cc26bb
Melissa Liu <meiliu1313@gmail.com>
2025-10-19 16:13:02 -0700

    chore: global styles

---
+++ styles/fonts.ts
@@ -0,0 +1,35 @@
+import localFont from "next/font/local";
+
+fonts: "use client";
+
+const Sans = localFont({
+  src: [
+    {
+      path: "../public/fonts/Public_Sans/PublicSans-Black.ttf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Public_Sans/PublicSans-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Public_Sans/PublicSans-Medium.ttf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Public_Sans/PublicSans-Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Public_Sans/PublicSans-ExtraBold.ttf",
+      weight: "400",
+      style: "italic",
+    },
+  ],
+});
+
+export { Sans };
