57584f6e63166514770b3ff9588bc0444133f4d3
Dylan Toh <dylantoh@berkeley.edu>
2025-11-13 21:31:05 -0800

    [feat] sign-in page

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,9 @@ export default function Home() {
     <main style={mainStyles}>
       <Image style={imageStyles} src={BPLogo} alt="Blueprint Logo" />
       <p>Open up app/page.tsx to get started!</p>
+      <Link href="/auth/sign-in">
+        <button>Sign-in/Sign-In</button>
+      </Link>
       <Link href="/auth/login-test">
         <button>Sign-in/Sign-up For Testing</button>
       </Link>
