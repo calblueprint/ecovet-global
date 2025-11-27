8e190af9712663ef63b838c0dbefe6156968f1d3
Cody Garcia <112846683+CodyGarciaa@users.noreply.github.com>
2025-11-27 00:54:02 -0800

    [feat] Made the Backend of Template Creation (#17)
    * basics of all the create functions, gonna read through the sprint again to see if that's it lol
    
    * started insertTemplateFlow, added createTemplates
    
    * functions now return their UUID; insertTemplateFlow now testing tempaltes, phases, and roles.
    
    * lowk should be done (first draft), havent tested at all though yet lol
    
    * some slight changes to match supabase, everything works as tested now
    
    * getting started on the backend. lowk started with some chatgpt code, but slowly revising and using it to help me know where to take the architecture of this backend. will prob have to add/edit some of the queries in the future to make it work.
    
    * yeah i think im gonna also make updates and delete queries now too to make implementation easier. also allowing basically everything to be nullable lol. wow this is lowk a hella confusing overhaul rip
    
    * i think finished updating schema + template queries to allow for updating and deleting. prob something small i forgot missing but will fix once i realize
    
    * cooked
    
    * more work on backend. general tabs are working, but nothing saves as of yet + no phase/prompt stuff
    
    * added template builder to separate the modulize the actual creation of a new template. can be more used now in the future outside of this one case.
    
    * kinda overhauled backend lol so it creates local ids and saves everything to local storage for now. from users standpoint, not too much progress. but backend should be a lot easier to work with going forward :)
    
    * working on phases and stuff. i think it mostly works, but nothing is shown yet so im not for sure. need to ammend role stuff now
    
    * role stuff should be working now. lowk everything is working as far as i know. started working on saving, have a proof of concept with description in roles saving working. nothing saving to supabase yet still.
    
    * yayyyyyyy phases dynamically add now finally. but bruh lol i totally forgot abt prompts, so gotta add like all of that now rip
    
    * so close to done with everything without saving. removing prompt is kinda broken, will fix tho
    
    * RAHHHH all functionality work. just gotta take the local storage now and save to supabase at the end. should be slight
    
    * yo crazy, it like all works first try. but testing i noticed i dont have phase descriptions rip. gonna add rn. but in terms of functionality for everything, like it worked off the bat.
    
    * everything works, added phase prompts.
    
    * ight this sprint should be done. not the prettiest cleaned code, but everything done. i wanna eat now
    
    * alrighttt u can remove phase from middle now
    
    * alright everything is prettier now
    
    * some comments, but lowk want to make pr first before i do anymore small edits
    
    * nothing
    
    * added immer
    
    * fixed duplication bc of immer
    
    * [chore]: saves template name, remove role fix
    
    ---------
    
    Co-authored-by: Melissa Liu <meiliu1313@gmail.com>

---
+++ app/templates/components/RoleForm.tsx
@@ -0,0 +1,106 @@
+import { UUID } from "crypto";
+import { roleFormInput, RolePhase } from "@/types/schema";
+
+export default function RoleForm({
+  value,
+  onChange,
+}: {
+  value: roleFormInput;
+  onChange: (id: UUID, field: string, v: string) => void;
+}) {
+  const rolePhases: RolePhase[] = [];
+  for (const [, rolePhaseID] of Object.entries(value.rolePhaseIndex)) {
+    rolePhases.push(value.rolePhases[rolePhaseID]);
+  }
+
+  return (
+    <div>
+      <fieldset>
+        <legend>Role Description</legend>
+        <input
+          type="text"
+          name="role_description"
+          placeholder="da descrition"
+          value={value.role.role_description ?? ""}
+          onChange={e =>
+            onChange(value.role.role_id, "role_description", e.target.value)
+          }
+        />
+      </fieldset>
+      {rolePhases.map((rolePhase, i) => (
+        <fieldset key={rolePhase.role_phase_id}>
+          <legend>Phase {i + 1}</legend>
+          <div style={{ display: "flex" }}>
+            <div>
+              <input
+                type="text"
+                name="role_phase_description"
+                placeholder="da descrition"
+                value={rolePhase.description ?? ""}
+                onChange={e =>
+                  onChange(
+                    rolePhase.role_phase_id,
+                    "description",
+                    e.target.value,
+                  )
+                }
+              />
+              <div
+                key={"div" + rolePhase.role_phase_id}
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  width: "25%",
+                }}
+              >
+                {value.promptIndex[rolePhase.role_phase_id].map(promptID => (
+                  <div key={"div" + promptID}>
+                    <input
+                      key={promptID}
+                      type="text"
+                      name="prompt"
+                      placeholder="da prompt"
+                      value={value.promptById[promptID].prompt_text ?? ""}
+                      onChange={e =>
+                        onChange(promptID, "prompt_text", e.target.value)
+                      }
+                    />
+                    <button
+                      onClick={() =>
+                        onChange(
+                          promptID,
+                          "remove_prompt",
+                          rolePhase.role_phase_id,
+                        )
+                      }
+                    >
+                      - Remove
+                    </button>
+                  </div>
+                ))}
+              </div>
+              <button
+                onClick={() =>
+                  onChange(
+                    rolePhase.role_phase_id,
+                    "add_prompt",
+                    rolePhase.role_phase_id,
+                  )
+                }
+              >
+                + New Prompt
+              </button>
+            </div>
+            <button
+              onClick={() =>
+                onChange(rolePhase.phase_id, "remove_phase", rolePhase.phase_id)
+              }
+            >
+              Trash
+            </button>
+          </div>
+        </fieldset>
+      ))}
+    </div>
+  );
+}
