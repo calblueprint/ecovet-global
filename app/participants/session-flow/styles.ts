84b71b8aa5af4593c01e6c00ecb43a608f2eef99
Melissa Liu <meiliu1313@gmail.com>
2026-01-11 00:34:37 -0800

    integrated prompt responses with sessions:

--- app/participants/session-flow/styles.ts
+++ app/participants/session-flow/styles.ts
@@ -68,28 +68,3 @@ export const StyledTextarea = styled(TextareaAutosize)`
     box-shadow: 0 0 0 2px #e8efff;
   }
 `;
-
-export const Button = styled.button`
-  width: 100%;
-  height: 45px;
-  gap: 10rem;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  align-self: stretch;
-  background-color: ${COLORS.darkElectricBlue};
-  font-family: ${Sans.style.fontFamily};
-  font-size: 12px;
-  font-style: normal;
-  font-weight: 700;
-  line-height: normal;
-  color: white;
-  cursor: pointer;
-  &:hover {
-    background-color: ${COLORS.darkElectricBlue};
-  }
-  border: 0px ${COLORS.darkElectricBlue};
-  opacity: ${({ disabled }) => (disabled ? 0.6 : 1)};
-  cursor: ${({ disabled }) => (disabled ? "not-allowed" : "pointer")};
-`;
