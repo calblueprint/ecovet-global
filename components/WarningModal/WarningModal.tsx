4a4b829ea6128cc5fc0943d1a358ce4e157074f1
Tanay <panttanay1@gmail.com>
2026-02-28 13:43:26 -0800

    [feat] Added warning modal to warn user before they delete an item. Testable through /sandbox link added to test the feature

---
+++ components/WarningModal/WarningModal.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import * as styles from './styles';
+
+export default function WarningModal({ open, onClose}: { open: boolean; onClose: (shouldDel: boolean) => void }) {
+
+  return (
+    <React.Fragment>
+      <Dialog
+        open={open}
+        onClose={onClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title" sx={styles.dialogTitle}>
+          {"Are you sure you want to delete this item?"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description" sx={styles.dialogCaption}>
+            This action cannot be undone.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          {/* shouldDel is false when cancel is pressed, true when delete is pressed */}
+          <Button sx={styles.cancelButton} onClick={() => onClose(false)}>Cancel</Button>
+          <Button sx={styles.deleteButton} onClick={() => onClose(true)}>Delete</Button>
+        </DialogActions>
+      </Dialog>
+    </React.Fragment>
+  );
+}
