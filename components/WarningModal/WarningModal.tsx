68e8e3a108bfe28296e591aef43ec144593e49b7
Tanay <panttanay1@gmail.com>
2026-03-07 15:41:14 -0800

    [fix] Changed styling of WarningModal to be styled components instead of CSS

--- components/WarningModal/WarningModal.tsx
+++ components/WarningModal/WarningModal.tsx
@@ -1,15 +1,12 @@
 import * as React from "react";
-import Button from "@mui/material/Button";
 import Dialog from "@mui/material/Dialog";
-import DialogActions from "@mui/material/DialogActions";
-import DialogContent from "@mui/material/DialogContent";
-import DialogContentText from "@mui/material/DialogContentText";
-import DialogTitle from "@mui/material/DialogTitle";
 import {
-  cancelButton,
-  deleteButton,
-  dialogCaption,
-  dialogTitle,
+  ButtonContainer,
+  CancelButton,
+  DeleteButton,
+  WarningCaption,
+  WarningDialog,
+  WarningTitle,
 } from "./styles";
 
 export default function WarningModal({
@@ -20,31 +17,23 @@ export default function WarningModal({
   onClose: (shouldDel: boolean) => void;
 }) {
   return (
-    <React.Fragment>
-      <Dialog
-        open={open}
-        onClose={onClose}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <DialogTitle id="alert-dialog-title" sx={dialogTitle}>
-          {"Are you sure you want to delete this item?"}
-        </DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-description" sx={dialogCaption}>
-            This action cannot be undone.
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          {/* shouldDel is false when cancel is pressed, true when delete is pressed */}
-          <Button sx={cancelButton} onClick={() => onClose(false)}>
-            Cancel
-          </Button>
-          <Button sx={deleteButton} onClick={() => onClose(true)}>
-            Delete
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </React.Fragment>
+    <Dialog
+      open={open}
+      onClose={onClose}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+      PaperComponent={WarningDialog}
+    >
+      <WarningTitle id="alert-dialog-title">
+        {"Are you sure you want to delete this item?"}
+      </WarningTitle>
+      <WarningCaption id="alert-dialog-description">
+        This action cannot be undone.
+      </WarningCaption>
+      <ButtonContainer>
+        <CancelButton onClick={() => onClose(false)}>Cancel</CancelButton>
+        <DeleteButton onClick={() => onClose(true)}>Delete</DeleteButton>
+      </ButtonContainer>
+    </Dialog>
   );
 }
