d8d9072a1858f17d8825ad0ff25b638c6fe2095c
eshabansiya <esha_bansiya@berkeley.edu>
2026-03-09 12:51:22 -0700

    fix duplicates

--- app/templates/components/RoleForm.tsx
+++ app/templates/components/RoleForm.tsx
@@ -43,20 +43,6 @@ export default function RoleForm({
           ></RolePhaseDescriptionInput>
         </RoleDescriptionTemplate>
       </RoleHeaderContainer>
-      <RoleHeaderContainer>
-        <RoleHeader>
-          <PhaseTemplateHeader>{phase.phase_name}</PhaseTemplateHeader>
-          <RoleTemplateName>{value.role.role_name}</RoleTemplateName>
-        </RoleHeader>
-
-        <RoleDescriptionTemplate>
-          <RolePhaseDescriptionInput
-            placeholder="Enter phase description here..."
-            value={rolePhase.description ?? ""}
-            onChange={e => onChange(rolePhaseId, "description", e.target.value)}
-          ></RolePhaseDescriptionInput>
-        </RoleDescriptionTemplate>
-      </RoleHeaderContainer>
 
       <PhaseCard key={rolePhase.role_phase_id}>
         {(value.promptIndex[rolePhase.role_phase_id] ?? []).map(
@@ -74,47 +60,18 @@ export default function RoleForm({
             </FieldCard>
           ),
         )}
-        <PhaseCard key={rolePhase.role_phase_id}>
-          {(value.promptIndex[rolePhase.role_phase_id] ?? []).map(
-            (promptID, j) => (
-              <FieldCard key={promptID}>
-                <FieldLegend>Question {j + 1}</FieldLegend>
-                <BigInput
-                  name="prompt"
-                  placeholder="Type question..."
-                  value={value.promptById[promptID].prompt_text ?? ""}
-                  onChange={e =>
-                    onChange(promptID, "prompt_text", e.target.value)
-                  }
-                />
-              </FieldCard>
-            ),
-          )}
-
-          <GhostButton
-            onClick={() =>
-              onChange(
-                rolePhase.role_phase_id,
-                "add_prompt",
-                rolePhase.role_phase_id,
-              )
-            }
-          >
-            + New Prompt
-          </GhostButton>
-        </PhaseCard>
-        <GhostButton
-          onClick={() =>
-            onChange(
-              rolePhase.role_phase_id,
-              "add_prompt",
-              rolePhase.role_phase_id,
-            )
-          }
-        >
-          + New Prompt
-        </GhostButton>
       </PhaseCard>
+      <GhostButton
+        onClick={() =>
+          onChange(
+            rolePhase.role_phase_id,
+            "add_prompt",
+            rolePhase.role_phase_id,
+          )
+        }
+      >
+        + New Prompt
+      </GhostButton>
     </FormStack>
   );
 }
