a265aee0c5186a5d341bbfac732b876183e0959f
Monique <nica14101@gmail.com>
2026-03-09 21:34:30 -0700

    scenario overview

--- app/participants/scenario-overview/page.tsx
+++ app/participants/scenario-overview/page.tsx
@@ -3,12 +3,13 @@
 import { useCallback, useEffect, useState } from "react";
 import { useParams, useRouter } from "next/navigation";
 import { UUID } from "crypto";
-import { fetchTemplateId } from "@/api/supabase/queries/sessions";
+import { fetchPhases, fetchTemplateId } from "@/api/supabase/queries/sessions";
 import { fetchTemplate } from "@/api/supabase/queries/templates";
-import { Template } from "@/types/schema";
+import { Phase, Template } from "@/types/schema";
 import {
   ButtonText,
   ContentBody,
+  ContentBody40,
   ContentBubble,
   ContentDiv,
   ContentHeader,
@@ -16,31 +17,48 @@ import {
   ContinueButtonDiv,
   Main,
   OverviewHeader,
+  PhaseDescriptionWrapper,
 } from "./styles";
 
-//fetchTemplateId(session_id: string)
 
 export default function ScenarioOverview() {
   const { sessionId } = useParams<{ sessionId: string }>();
 
-  //const [templateId, setTemplateId] = useState< UUID | null>(null);
+  
   const [templateInfo, setTemplateInfo] = useState<Template | null>(null);
-  //const router = useRouter();
+  const [phases, setPhases] = useState<Phase[]>([]);
+  const [phaseInd, setPhaseInd] = useState(-1);
 
   const loadData = useCallback(async () => {
     if (!sessionId) return;
-    let templateId = await fetchTemplateId(sessionId as UUID);
+    const templateId = await fetchTemplateId(sessionId as UUID);
     const template = await fetchTemplate(templateId as unknown as UUID);
     setTemplateInfo(template);
+    setPhases(await fetchPhases(sessionId));
   }, [sessionId]);
 
   useEffect(() => {
     loadData();
   }, [loadData]);
 
+  function nextPhase() {
+    if (phaseInd >= phases.length) return;
+    setPhaseInd(phaseInd+1);
+  }
+
   return (
     <Main>
       <ContentDiv>
+        <PhaseDescriptionWrapper phase={phaseInd > -1}>
+          <ContentBody40>{phaseInd + 1} of {phases.length}</ContentBody40>
+          <OverviewHeader>Phase {phaseInd + 1}</OverviewHeader>
+          <ContentBubble>
+            <ContentHeader>Context</ContentHeader>
+            <ContentBody>
+              {phases.length > 0 ? phases[phaseInd].phase_description : "no phase"}
+            </ContentBody>
+          </ContentBubble>
+        </PhaseDescriptionWrapper>
         <OverviewHeader>Scenario Overview</OverviewHeader>
         <ContentBubble>
           <ContentHeader>Summary</ContentHeader>
@@ -55,10 +73,13 @@ export default function ScenarioOverview() {
           </ContentBody>
         </ContentBubble>
         <ContinueButtonDiv>
-          <ContinueButton>
+          <ContinueButton onClick={nextPhase} disabled={phaseInd >= phases.length}>
             <ButtonText>Continue</ButtonText>
           </ContinueButton>
         </ContinueButtonDiv>
+      </ContentDiv>
+      <ContentDiv>
+        
       </ContentDiv>
     </Main>
   );
