f7af08113504088f5ecf6f4b3942518b00c356f2
Tanay Pant <panttanay1@gmail.com>
2026-03-27 20:36:02 -0700

    Tanay/session start (#88)
    * [feat] Beginning exercises/start, added invite component and topnav
    
    * [feat] Basic structure of exercises/start complete, need to make selected participants a dropdown, implement add button, and more
    
    * [feat] mock data entered and working properly, now need to connect to supabase for the real data
    
    * [fix] changed the dropdown to be the base inputdropdown, now need to implement styling and design fixes so that it looks like the figma
    
    * [fix]  implemented styling and design fixes for the dropdown menus
    
    * added key strokes
    
    * [fix] add fetchTemplates to templates.ts
    
    * [fix] exercise dropdown now correctly calls from supabase
    
    * [fix] role selection now calls correctly based on exercise, no longer the default hardcoded values from before
    
    * [fix] styling fixes for font weight (selected participants, role table headers, sync/async boldness, select excerise dropdown height)
    
    * [fix] some styling changes to align with josh's comments
    
    * fix small styling and add delete button
    
    * fix delete bug
    
    ---------
    
    Co-authored-by: eshabansiya <esha_bansiya@berkeley.edu>

--- components/InputDropdown/InputDropdown.tsx
+++ components/InputDropdown/InputDropdown.tsx
@@ -1,7 +1,8 @@
 "use client";
 
+import type { SelectInstance } from "react-select";
 import { useCallback, useId, useMemo } from "react";
-import Select, { MultiValue, SingleValue } from "react-select";
+import Select, { MultiValue, SingleValue, StylesConfig } from "react-select";
 import { DropdownOption } from "@/types/dropdown";
 import { selectStyles } from "./styles";
 
@@ -13,6 +14,9 @@ interface CommonProps {
   error?: string;
   disabled?: boolean;
   required?: boolean;
+  customStyles?: StylesConfig<DropdownOption, boolean>;
+  onKeyDown?: (e: React.KeyboardEvent) => void;
+  selectRef?: React.Ref<SelectInstance<DropdownOption>>;
 }
 
 interface MultiSelectProps extends CommonProps {
@@ -26,7 +30,6 @@ interface SingleSelectProps extends CommonProps {
 }
 
 type InputDropdownProps = SingleSelectProps | MultiSelectProps;
-
 // main dropdown component
 export default function InputDropdown({
   options,
@@ -35,6 +38,9 @@ export default function InputDropdown({
   required,
   onChange,
   multi,
+  customStyles,
+  onKeyDown,
+  selectRef,
 }: InputDropdownProps) {
   const optionsArray = useMemo(
     () =>
@@ -62,8 +68,11 @@ export default function InputDropdown({
 
   return (
     <Select
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      ref={selectRef as React.Ref<any>}
+      onKeyDown={onKeyDown}
       isClearable
-      closeMenuOnSelect={false}
+      closeMenuOnSelect={multi ? false : true}
       tabSelectsValue={false}
       hideSelectedOptions={false}
       required={required}
@@ -73,7 +82,9 @@ export default function InputDropdown({
       placeholder={placeholder}
       isMulti={multi}
       onChange={handleChange}
-      styles={selectStyles}
+      styles={
+        customStyles || (selectStyles as StylesConfig<DropdownOption, boolean>)
+      }
     />
   );
 }
