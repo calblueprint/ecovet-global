30af9862d7bf99b0bef0277471dad40fd56eba56
eshabansiya <esha_bansiya@berkeley.edu>
2026-04-10 13:47:53 -0700

    fix send email to self

--- app/send-emails/page.tsx
+++ app/send-emails/page.tsx
@@ -3,15 +3,19 @@
 import { useEffect, useState } from "react";
 import { fetchProfileByUserId } from "@/actions/supabase/queries/profile";
 import { sendEmailReminder } from "@/actions/supabase/send-email";
-import { Profile } from "@/types/schema";
 import { useProfile } from "@/utils/ProfileProvider";
 
 export default function SendEmailButton() {
   const { profile } = useProfile();
-  const [message, setMessage] = useState<string>("");
   const [email, setEmail] = useState<string>("");
-  const [name, setName] = useState<string>("");
-  const [userProfile, setUserProfile] = useState<Profile | null>(null);
+  const [message, setMessage] = useState<string>("");
+
+  useEffect(() => {
+    if (!profile?.id) return;
+    fetchProfileByUserId(profile.id).then(p => {
+      if (p?.email) setEmail(p.email);
+    });
+  }, [profile?.id]);
 
   const sendEmail = async () => {
     if (!email) {
@@ -20,20 +24,6 @@ export default function SendEmailButton() {
     }
     setMessage(await sendEmailReminder(email));
   };
-
-  useEffect(() => {
-    if (!profile?.id) return;
-
-    async function fetchNameandEmail(user_id: string) {
-      setUserProfile(await fetchProfileByUserId(user_id));
-      if (!userProfile) return;
-      if (!userProfile.email) return;
-      setEmail(userProfile.email);
-      setName(userProfile.first_name ? userProfile.first_name : "");
-    }
-
-    fetchNameandEmail(profile.id);
-  }, [profile?.id, profile]);
 
   return (
     <div>
